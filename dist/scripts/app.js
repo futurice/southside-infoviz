1c29629f9041519953e964d92479d97ad2131d90
Ossi Hanhinen <ossi.hanhinen@gmail.com>
2015-03-13 13:47:22 +0200

    Update dist

---
+++ dist/scripts/app.js
@@ -0,0 +1,1 @@
+!function(o){"use strict";var e=o.querySelector("#app");e.jsonUrl="mock/mockdata.json",e.addEventListener("template-bound",function(){console.log("Our app is ready to rock!")})}(wrap(document));
